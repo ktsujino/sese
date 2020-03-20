3310be0e07b0cb90eca83c58ca4fb65dbbf61b47
Kosuke Tsujino <kosuke.tsujino@gmail.com>
2020-03-20 20:04:14 +0900

    Revert "changed http_server_async.cc to return strings"
    This reverts commit b4990aec7ffd7b28b03a52f1cd1f58d20e616dc1.

--- http_server_async.cc
+++ http_server_async.cc
@@ -153,17 +153,53 @@ handle_request(
         req.method() != http::verb::head)
         return send(bad_request("Unknown HTTP-method"));
 
-    if( req.target().size() < 2 ||
+    // Request path must be absolute and not contain "..".
+    if( req.target().empty() ||
         req.target()[0] != '/' ||
-	req.target()[1] != '?')
+        req.target().find("..") != beast::string_view::npos)
         return send(bad_request("Illegal request-target"));
 
-    http::response<http::string_body> res{http::status::ok, req.version()};
+    // Build the path to the requested file
+    std::string path = path_cat(doc_root, req.target());
+    if(req.target().back() == '/')
+        path.append("index.html");
+
+    // Attempt to open the file
+    beast::error_code ec;
+    http::file_body::value_type body;
+    body.open(path.c_str(), beast::file_mode::scan, ec);
+
+    // Handle the case where the file doesn't exist
+    if(ec == beast::errc::no_such_file_or_directory)
+        return send(not_found(req.target()));
+
+    // Handle an unknown error
+    if(ec)
+        return send(server_error(ec.message()));
+
+    // Cache the size since we need it after the move
+    auto const size = body.size();
+
+    // Respond to HEAD request
+    if(req.method() == http::verb::head)
+    {
+        http::response<http::empty_body> res{http::status::ok, req.version()};
+        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+        res.set(http::field::content_type, mime_type(path));
+        res.content_length(size);
+        res.keep_alive(req.keep_alive());
+        return send(std::move(res));
+    }
+
+    // Respond to GET request
+    http::response<http::file_body> res{
+        std::piecewise_construct,
+        std::make_tuple(std::move(body)),
+        std::make_tuple(http::status::ok, req.version())};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-    res.set(http::field::content_type, "application/json");
+    res.set(http::field::content_type, mime_type(path));
+    res.content_length(size);
     res.keep_alive(req.keep_alive());
-    res.body() = std::string(req.target());
-    res.prepare_payload();
     return send(std::move(res));
 }
 
